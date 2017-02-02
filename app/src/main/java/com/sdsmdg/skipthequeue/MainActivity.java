202ad5963b463dd65024fec34ec49582c9b48d1d
Yash <ysh.kant@gmail.com>
2017-02-03 03:45:29 +0530

    Kontakt SDK added.

--- app/src/main/java/com/sdsmdg/skipthequeue/MainActivity.java
+++ app/src/main/java/com/sdsmdg/skipthequeue/MainActivity.java
@@ -2,12 +2,89 @@ package com.sdsmdg.skipthequeue;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.kontakt.sdk.android.ble.configuration.ScanMode;
+import com.kontakt.sdk.android.ble.connection.OnServiceReadyListener;
+import com.kontakt.sdk.android.ble.manager.ProximityManager;
+import com.kontakt.sdk.android.ble.manager.ProximityManagerFactory;
+import com.kontakt.sdk.android.ble.manager.listeners.EddystoneListener;
+import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleEddystoneListener;
+import com.kontakt.sdk.android.ble.spec.EddystoneFrameType;
+import com.kontakt.sdk.android.common.KontaktSDK;
+import com.kontakt.sdk.android.common.profile.IEddystoneDevice;
+import com.kontakt.sdk.android.common.profile.IEddystoneNamespace;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProximityManager proximityManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        KontaktSDK.initialize("MyaKMlUTJVkvgLOdCNHDvIrCtZmNsJEL");
+        proximityManager = ProximityManagerFactory.create(this);
+        proximityManager.setEddystoneListener(createEddystoneListener());
+        configureProximityManager();
+
+        //Add the snackbar code.
+
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startScanning();
+    }
+
+    @Override
+    protected void onStop() {
+        proximityManager.stopScanning();
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        proximityManager.disconnect();
+        proximityManager = null;
+        super.onDestroy();
+    }
+
+    private void startScanning() {
+        proximityManager.connect(new OnServiceReadyListener() {
+            @Override
+            public void onServiceReady() {
+                proximityManager.startScanning();
+            }
+        });
+    }
+
+    private EddystoneListener createEddystoneListener() {
+        return new SimpleEddystoneListener() {
+            @Override
+            public void onEddystoneDiscovered(IEddystoneDevice eddystone, IEddystoneNamespace namespace) {
+                Log.i("Sample", "Eddystone discovered: " + eddystone.toString());
+            }
+
+            @Override
+            public void onEddystoneLost(IEddystoneDevice eddystone, IEddystoneNamespace namespace) {
+                super.onEddystoneLost(eddystone, namespace);
+                Log.i("Sample", "Eddystone lost: " + eddystone.toString());
+
+            }
+
+        };
+    }
+
+    private void configureProximityManager() {
+        proximityManager.configuration()
+                .scanMode(ScanMode.LOW_LATENCY)
+                .eddystoneFrameTypes(Arrays.asList( EddystoneFrameType.URL));
     }
 }
