04caf1af11eb6928249b624cfc18c4568c5baa45
harshit211997 <harshit211997@gmail.com>
2017-02-06 20:05:37 +0530

    :art: ui changes

--- app/src/main/java/com/sdsmdg/skipthequeue/StartingActivity.java
+++ app/src/main/java/com/sdsmdg/skipthequeue/StartingActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings;
 import android.support.design.widget.Snackbar;
@@ -17,9 +18,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -140,7 +139,6 @@ public class StartingActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext() , String.valueOf(beaconsArray.size()),Toast.LENGTH_SHORT).show();
                 beaconAdapter = new BeaconAdapter(getApplicationContext(),beaconsArray);
                 beaconsListView.setAdapter(beaconAdapter);
-
             }
         };
     }
