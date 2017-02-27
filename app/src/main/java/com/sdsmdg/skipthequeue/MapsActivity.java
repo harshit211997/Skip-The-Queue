6f3b6c9150918101051b443c51083f3f46e79ef3
Yash <ysh.kant@gmail.com>
2017-02-28 00:16:57 +0530

    Route Between the markers added.

--- app/src/main/java/com/sdsmdg/skipthequeue/MapsActivity.java
+++ app/src/main/java/com/sdsmdg/skipthequeue/MapsActivity.java
@@ -1,7 +1,12 @@
 package com.sdsmdg.skipthequeue;
 
+import android.app.ProgressDialog;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,6 +15,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.sdsmdg.skipthequeue.Maps.JSONParser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -43,8 +58,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
         //Declare the points.
-        LatLng ATM = new LatLng(getIntent().getDoubleExtra("lat",21) + .01, getIntent().getDoubleExtra("lng",57) +.01);
-        LatLng user = new LatLng(getIntent().getDoubleExtra("lat",21), getIntent().getDoubleExtra("lng",57));
+
+        Double userLat = getIntent().getDoubleExtra("lat",21);
+        Double userLng =  getIntent().getDoubleExtra("lng",57);
+        Double atmLat = getIntent().getDoubleExtra("lat",21) + .01;
+        Double atmLng = getIntent().getDoubleExtra("lng",57) + .01;
+
+        LatLng ATM = new LatLng(atmLat,atmLng);
+        LatLng user = new LatLng(userLat,userLng);
 
         mMap.addMarker(new MarkerOptions().position(user).title("Your current Position."));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(user));
@@ -56,8 +77,144 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .include(user).build();
         mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 200));
 
-        //Adding the code to draw route between two markers here.
+        //Adding the code to draw route between two markers.
+        String urlPass = makeURL(userLat,userLng, atmLat ,atmLng);
+        ConnectAsyncTask asyncTask = new ConnectAsyncTask(urlPass);
+        asyncTask.execute();
+
 
 
     }
+
+    public String makeURL (double sourcelat, double sourcelog, double destlat, double destlog ){
+        StringBuilder urlString = new StringBuilder();
+        urlString.append("https://maps.googleapis.com/maps/api/directions/json");
+        urlString.append("?origin=");// from
+        urlString.append(Double.toString(sourcelat));
+        urlString.append(",");
+        urlString
+                .append(Double.toString( sourcelog));
+        urlString.append("&destination=");// to
+        urlString
+                .append(Double.toString( destlat));
+        urlString.append(",");
+        urlString.append(Double.toString( destlog));
+        urlString.append("&sensor=false&mode=driving&alternatives=true");
+        urlString.append("&key=AIzaSyCzMYoH0gJDstUbcWsicITDcIURScd42Y0");
+        Log.i("TAG", "makeURL: "+ urlString.toString());
+        return urlString.toString();
+
+    }
+
+    public void drawPath(String  result) {
+
+        try {
+            //Tranform the string into a json object
+            final JSONObject json = new JSONObject(result);
+            JSONArray routeArray = json.getJSONArray("routes");
+            JSONObject routes = routeArray.getJSONObject(0);
+            JSONObject overviewPolylines = routes.getJSONObject("overview_polyline");
+            String encodedString = overviewPolylines.getString("points");
+            List<LatLng> list = decodePoly(encodedString);
+
+            Polyline line = mMap.addPolyline(new PolylineOptions()
+                    .addAll(list)
+                    .width(12)
+                    .color(Color.parseColor("#05b1fb"))//Google maps blue color
+                    .geodesic(true)
+            );
+
+
+            /*
+           for(int z = 0; z<list.size()-1;z++){
+                LatLng src= list.get(z);
+                LatLng dest= list.get(z+1);
+                Polyline line = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(src.latitude, src.longitude), new LatLng(dest.latitude,   dest.longitude))
+                .width(2)
+                .color(Color.BLUE).geodesic(true));
+            }
+           */
+        }
+        catch (JSONException e) {
+
+        }
+    }
+
+    private List<LatLng> decodePoly(String encoded) {
+
+        List<LatLng> poly = new ArrayList<LatLng>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng( (((double) lat / 1E5)),
+                    (((double) lng / 1E5) ));
+            poly.add(p);
+        }
+
+        return poly;
+    }
+
+    private class ConnectAsyncTask extends AsyncTask<Void, Void, String> {
+        private ProgressDialog progressDialog;
+        String url;
+        ConnectAsyncTask(String urlPass){
+            url = urlPass;
+        }
+        @Override
+        protected void onPreExecute() {
+            // TODO Auto-generated method stub
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(MapsActivity.this);
+            progressDialog.setMessage("Fetching route, Please wait...");
+            progressDialog.setIndeterminate(true);
+            progressDialog.show();
+        }
+        @Override
+        protected String doInBackground(Void... params) {
+            JSONParser jParser = new JSONParser();
+            String json = jParser.getJSONFromUrl(url);
+            Log.i("TAG", "doInBackground: " + json);
+            return json;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            progressDialog.hide();
+            if(result!=null){
+
+                Toast.makeText(getApplicationContext(),"Path Available", Toast.LENGTH_SHORT).show();
+                drawPath(result);
+            }
+
+            else
+            {
+                Toast.makeText(getApplicationContext(),"Path Not Available", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+
+
 }
